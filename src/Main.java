fc2ef123341fd7e4c0765534a94cf882fab58747
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 15:40:42 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Hjölp en lärare att ta fram statistik om årets prov från filen prov.txt
+        //  - Spara namnen i en endimensionell array
+        //  - Spara provdata i en 2D-array
+        //
+        //  - Skriv ut varje students namn och genomsnittspoäng
+        //  - Skriv för varje prov ut genomsnittspoängen (för alla studenter)
+
+
+    }
+}
