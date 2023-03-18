bdd2985410209e2757acc92ed92f69cd30a7496b
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 16:09:56 +0100

    Fixed spelling mistake

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Hjölp en lärare att ta fram statistik om årets prov från filen prov.txt
+        // Hjälp en lärare att ta fram statistik om årets prov från filen prov.txt
         //  - Spara namnen i en endimensionell array
         //  - Spara provdata i en 2D-array
         //
