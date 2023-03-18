db7989413c8f1f82d900bc7b0df5b4881207d979
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 16:38:47 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // Hjälp en lärare att ta fram statistik om årets prov från filen prov.txt
         //  - Spara namnen i en endimensionell array
         //  - Spara provdata i en 2D-array
@@ -7,6 +11,36 @@ public class Main {
         //  - Skriv ut varje students namn och genomsnittspoäng
         //  - Skriv för varje prov ut genomsnittspoängen (för alla studenter)
 
+        int nbrStudents = 0;
+        int nbrTests = 5;
+        String[] names = new String[30];
+        int[][] grades = new int[30][5];
 
+        Scanner fileIn = new Scanner(new File("prov.txt"));
+        while (fileIn.hasNextLine()) {
+            names[nbrStudents] = fileIn.nextLine();
+            for (int i=0; i < nbrTests; i++) {
+                grades[nbrStudents][i] = fileIn.nextInt();
+            }
+            fileIn.nextLine();
+            nbrStudents++;
+        }
+
+        for (int i=0; i < nbrStudents; i++) {
+            double sum = 0;
+            for (int j=0; j < nbrTests; j++) {
+                sum += grades[i][j];
+            }
+            System.out.format("Genomsnittspoäng för %s: %.2f%n", names[i], (sum/nbrTests));
+        }
+
+        System.out.println();
+        for (int i=0; i < nbrTests; i++) {
+            double sum = 0;
+            for (int j=0; j < nbrStudents; j++) {
+                sum += grades[j][i];
+            }
+            System.out.format("Genomsnittspoäng för test %d: %.2f%n", i, (sum/nbrStudents));
+        }
     }
 }
